3113f683c822508d2b66bdb57c4bd1b1636bb0f3
somallg <tq.duong@icloud.com>
2014-11-17 19:51:16 +0700

    Update GotAAdventure.js

--- GotA/GotAAdventure.js
+++ GotA/GotAAdventure.js
@@ -18,6 +18,23 @@ function countDownSS() {
 	console.log("======================================================");
 }
 
+function breadCountDown() {
+	var sept = userContext.buildingsData[6];
+	sept.build_remaining -= 35;
+	console.log("Bread id " + sept.item_id + " duration remaining " + sept.build_remaining);
+	
+	if (sept.build_remaining < 0) {
+		console.log("doFinishProduction");
+		doFinishProduction(sept.item_id);
+		if (sept.build_remaining == null) {
+			console.log("doProduction");
+			doProduction('fresh_baked_bread','sept',undefined,undefined,'sept_fresh_baked_bread_recipe');
+		}
+	}
+	console.log("breadCountDown DONE");
+}
+
 var advInterval = setInterval(function() {
 	countDownSS();
+	breadCountDown();
 }, 3E4);
